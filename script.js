b62023924ac9d3d75c7561936d19773f704accc8
RohitX <rohit10chandra@gmail.com>
2025-04-08 12:03:56 +0530

    Update script.js
    removed api key

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyAyO01JLy0g_6y_faZKd3Eque3TYe5kKIM";
+const API_KEY = "Your_Google_API_Key";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
